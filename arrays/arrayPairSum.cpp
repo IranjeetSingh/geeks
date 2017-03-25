16629841f8e71169489b323f20d4aba53fa8a0c4
Iranjeet Singh <iranjeetsingh@Iranjeets-MacBook-Pro.local>
2017-03-25 19:12:50 +0530

    array pair sum added

---
+++ arrays/arrayPairSum.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+void printPair( int a[], int size, int sum )
+{
+  bool *isPresent = new bool[50] ;
+
+  int x = 0 ;
+  for( int i = 0 ; i < size ; ++i ) 
+  {
+    x = sum - a[i] ;
+
+    if( (x > 0 ) && (isPresent[(x)]) )
+    {
+       std::cout << x << " " << a[i] << " " << std::endl ;
+    }
+    
+    isPresent[a[i]] = true ;
+  }
+}
+
+
+int main()
+{
+
+  int a[]={1, 4, 45, 6, 10, 8};
+  int sum = 16 ;
+  int size = sizeof(a)/sizeof(a[0]);
+  printPair( a, size, sum ) ;
+}
